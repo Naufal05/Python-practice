586c5d95586a53b9d81630d5e642ad60059e8402
NaufalMZ05 <mnaufalmz@gmail.com>
2026-06-12 18:32:13 +0530

    16-Turtle graphics- OBjects

---
+++ 16/main.py
@@ -0,0 +1,11 @@
+from turtle import Turtle, Screen
+
+jonhny = Turtle()
+print(jonhny)
+jonhny.shape("turtle")
+jonhny.color("red")
+jonhny.forward(100)
+
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
