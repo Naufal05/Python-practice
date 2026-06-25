eb21a3766c30ae7137e61eb80025f1f859c291e7
NaufalMZ05 <mnaufalmz@gmail.com>
2026-06-25 12:22:25 +0530

    person.py

---
+++ OOPS/oops_intro/person.py
@@ -0,0 +1,13 @@
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def greet(self):
+        print(f"Hello, my name is {self.name} and I am {self.age} years old.")
+
+
+person1 = Person("Alice", 20)
+person1.greet()
+person2 = Person("BOB", 20)
+person2.greet()
