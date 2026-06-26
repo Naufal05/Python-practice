481136c44634c200ae2f8172ca558338882f49b9
NaufalMZ05 <mnaufalmz@gmail.com>
2026-06-26 17:14:19 +0530

    Staitc and instance mehtod-advance - protective, private etc

--- OOPS/oops_intro/static.py
+++ OOPS/oops_intro/static.py
@@ -26,3 +26,33 @@
 
 class BankAccount:
     MIN_BALANCE = 100
+
+    def __init__(self, owner, balance = 0):
+        self.owner = owner
+        self._balance = balance
+
+    def deposit(self, amount):
+        if self._is_valid_amount(amount):
+            self._balance += amount
+            # print(f"{self.owner}'s new balance: ${self._balance}")
+            self.__log_transaction("deposit", amount)
+        else:
+            print("Deposit amount must be positive.")
+            
+    # protective method
+    def _is_valid_amount(self, amount):
+        return amount > 0
+    
+    # private methos - accessible with in internal class, cannot be overwritten 
+    def __log_transaction(self, transactionType, amount):
+        print(f"Logging {transactionType} of ${amount}. New balance: ${self._balance}")
+
+    @staticmethod
+    def is_valid_interest_rate(rate):
+        return 0 <= rate <= 5
+    
+
+account = BankAccount("John", 500)
+account.deposit(200)
+
+print(BankAccount.is_valid_interest_rate(3))
