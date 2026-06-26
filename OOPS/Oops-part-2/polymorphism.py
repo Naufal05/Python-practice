cc43df0ea5b72aa3f913d971e7663666696c6f71
NaufalMZ05 <mnaufalmz@gmail.com>
2026-06-27 02:17:52 +0530

    polymorphism example

---
+++ OOPS/Oops-part-2/polymorphism.py
@@ -0,0 +1,22 @@
+# POLYMORPHISM
+
+class Vehicle:
+    def __init__(self, brand, model, year):
+        self.brand = brand
+        self.model = model
+        self.year = year
+
+    def start(self):
+        print("Vehcile is starting")
+
+    def stop(self):
+        print("Vehicle is stoping")
+    
+
+# create a list vehicles to inspect
+vehicles: list[Vehicle1 ] = [
+
+]
+
+
+# Loop through list of vehicles and insepct them
