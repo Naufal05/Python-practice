7fe92dfad5e54f57009cbe2e80791017ec44565c
NaufalMZ05 <mnaufalmz@gmail.com>
2026-07-06 01:10:16 +0530

    paddle class

--- 21-pong-game/main.py
+++ 21-pong-game/main.py
@@ -1,4 +1,6 @@
 from turtle import Screen, Turtle
+from paddle import Paddle
+
 
 screen = Screen()
 screen.title("Pong game")
@@ -6,12 +8,8 @@ screen.bgcolor("black")
 screen.setup(width=800, height=600)
 screen.tracer(0)
 
-paddle = Turtle()
-paddle.shape("square")
-paddle.color("white")
-paddle.shapesize(stretch_wid=5, stretch_len=1)
-paddle.penup()
-paddle.goto(350, 0)
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
 
 
 def go_up():
