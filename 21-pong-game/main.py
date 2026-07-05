b1fd2a9b9e75ba7448aa01c41da0c9cce5c770c0
NaufalMZ05 <mnaufalmz@gmail.com>
2026-07-06 01:53:56 +0530

    Ball class created and initalised

--- 21-pong-game/main.py
+++ 21-pong-game/main.py
@@ -1,6 +1,7 @@
 from turtle import Screen, Turtle
 from paddle import Paddle
-
+from ball import Ball
+import time
 
 screen = Screen()
 screen.title("Pong game")
@@ -11,24 +12,20 @@ screen.tracer(0)
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
 
-
-def go_up():
-    new_y = paddle.ycor() + 20
-    paddle.goto(paddle.xcor(), new_y)
-
-def go_down():
-    new_y = paddle.ycor() - 20
-    paddle.goto(paddle.xcor(), new_y)
+ball = Ball()
 
 
 screen.listen()
-screen.onkey(go_up, "Up")
-screen.onkey(go_down, "Down")
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
 
 
 game_is_on = True
 while game_is_on:
     screen.update()
-
+    time.sleep(0.1)
+    ball.move()
       
 screen.exitonclick()
