e93197baaa361f3fbfd4e76eb92c886777bac85f
NaufalMZ05 <mnaufalmz@gmail.com>
2026-07-06 13:07:37 +0530

    pong game completed

--- 21-pong-game/main.py
+++ 21-pong-game/main.py
@@ -1,6 +1,7 @@
 from turtle import Screen, Turtle
 from paddle import Paddle
 from ball import Ball
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
@@ -13,7 +14,7 @@ r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
 
 ball = Ball()
-
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(r_paddle.go_up, "Up")
@@ -25,7 +26,7 @@ screen.onkey(l_paddle.go_down, "s")
 game_is_on = True
 while game_is_on:
     screen.update()
-    time.sleep(0.1)
+    time.sleep(ball.move_speed)
     ball.move()
     
     # Detect collision with wall
@@ -39,8 +40,13 @@ while game_is_on:
     # Detect when the right paddle misses
     if ball.xcor() > 380:
         ball.reset_position()
+        scoreboard.l_point()
 
-    if ball.xcor() > -380:
+    if ball.xcor() < -380:
         ball.reset_position()
+        scoreboard.r_point()
+
+    #detect when tehe right paddle misses
+
 
 screen.exitonclick()
